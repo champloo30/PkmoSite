07f62d23d3673f70c15c55962c9df861886c4aa6
champloo30 <djld30@gmail.com>
2021-11-15 22:19:50 -0500

    finished landing page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
 import '../../scss/footer.scss'
+import facebook from "../../img-icon/icons8-facebook.svg"
+import youtube from "../../img-icon/icons8-youtube.svg"
 
 export default function Footer() {
     return (
         <div className="footer" id="footer">
+            <div className="logo"></div>
+            <h2>Peace Kingdom Ministries of Ohio</h2>
+            <a href="http://www.facebook.com/PeaceMissionaryBaptistChurch" target="_blank" rel="noopener noreferrer">
+                <img className="icon" src={facebook} alt="Facebook Logo" />
+            </a>
+            <a href="http://www.youtube.com/user/PeaceMBChurch" target="_blank" rel="noopener noreferrer">
+                <img className="icon" src={youtube} alt="Youtube Logo" />
+            </a>
             
         </div>
     )
