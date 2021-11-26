d93e32597ce6f2beeae00b8295d4cfdd232dcddc
champloo30 <djld30@gmail.com>
2021-11-25 22:07:57 -0600

    finished bible teaching and church online page

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -34,7 +34,11 @@ export default function Menu({ menuOpen, setMenuOpen }) {
                         <li onClick={()=>setMenuOpen(false)}>
                             <Link className="link" to="/bibleteachings">Bible Teachings</Link>
                         </li>
-                        <li className="link">Church Online</li>
+                        <li onClick={()=>setMenuOpen(false)}>
+                            <a className="link" href="http://peacekmo.online.church" target="_blank" rel="noopener noreferrer">
+                                Church Online
+                            </a>
+                        </li>
                     </li>
                     <li className="link-name">
                         <div className="title">
