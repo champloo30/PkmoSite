7cc5fb9982f0281ed0ec2051fff75ff67ec0a44f
champloo30 <djld30@gmail.com>
2021-12-11 22:39:44 -0500

    regular commit

--- src/components/about/Leadership.jsx
+++ src/components/about/Leadership.jsx
@@ -14,7 +14,8 @@ export default function Leadership() {
                         <h2>Dalyn L. Dunn</h2>
                     </div>
                     <div className="text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus ut faucibus pulvinar elementum integer enim neque. Elementum integer enim neque volutpat ac tincidunt. Duis convallis convallis tellus id interdum velit. Suscipit tellus mauris a diam maecenas sed enim ut. Etiam sit amet nisl purus in mollis nunc. Sapien et ligula ullamcorper malesuada proin. Tempor nec feugiat nisl pretium fusce. Interdum velit euismod in pellentesque. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Ullamcorper malesuada proin libero nunc.</p>
+                        <p>Pastor Dalyn L Dunn, is a native of Columbus, Ohio until his recent relocating to southern Georgia November 2021. He is the eldest of 6 boys and embraces the healthy family values of a blended family dynamic.</p>
+                        
                     </div>
                 </div>
                 <div className="bio-cp">
