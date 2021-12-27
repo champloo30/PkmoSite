80414ba5c44fdf7d982739590e63ab69b0d70da4
champloo30 <djld30@gmail.com>
2021-12-26 21:05:39 -0500

    FINALLY finished linking html form to google sheets

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -1,6 +1,53 @@
 import "../../scss/connect.scss"
+import { useState } from "react";
 
 export default function Connect() {
+    const [data, setData] = useState({
+        fname: "",
+        lname: "",
+        phone: "",
+        email: "",
+        option: "",
+        text: "",
+    });
+
+    const { fname, lname, phone, email, option, text } = data;
+
+    const handleChange = e => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                'https://v1.nocodeapi.com/champloo30/google_sheets/vqfYVNYpdvYYGPlN?tabId=Connect', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify([
+                        [fname, lname, phone, email, option, text, new Date().toLocaleString()],
+                    ]),
+                }
+            );
+                await response.json();
+                setData({ 
+                    ...data, 
+                    fname: "", 
+                    lname: "", 
+                    phone: "", 
+                    email: "", 
+                    option: "", 
+                    text:"" 
+                });
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
             <div className="connect">
@@ -11,16 +58,34 @@ export default function Connect() {
                     <p>Please fill out the form and we'll get you in touch with the right people to provide the right service that you need!</p>
                 </div>
                 <div className="formbox">
-                    <form className="form" action="" method="post">
+                    <form className="form" onSubmit={handleSubmit}>
                         <h3>Get Connected With The Kingdom</h3>
                         <div className="form-name">
                             <div className="fname">
                                 <label htmlFor="fname">First Name</label><br />
-                                <input type="text" name="fname" id="lname" placeholder="John" required/><br />
+                                <input 
+                                    type="text" 
+                                    name="fname" 
+                                    id="fname" 
+                                    placeholder="John" 
+                                    value={fname}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <br />
                             </div>
                             <div className="lname">
                                 <label htmlFor="lname">Last Name</label><br />
-                                <input type="text" name="lname" id="lname" placeholder="Doe" required/><br />
+                                <input 
+                                    type="text" 
+                                    name="lname" 
+                                    id="lname" 
+                                    placeholder="Doe" 
+                                    value={lname}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <br />
                             </div>
                         </div>
                         <div className="phone-email">
@@ -31,46 +96,112 @@ export default function Connect() {
                                         <input className="checkbox" type="checkbox" name="phone-email" id="phone" value="Phone" minLength="10" maxLength="14" />
                                         <label htmlFor="phone">Phone Number</label><br />
                                     </div>
-                                    <input className="text" type="tel" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="999-999-9999"/><br />
+                                    <input 
+                                        className="text" 
+                                        type="tel" 
+                                        name="phone" 
+                                        id="phone" 
+                                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+                                        placeholder="999-999-9999"
+                                        value={phone}
+                                        onChange={handleChange}
+                                    />
+                                    <br />
                                 </div>
                                 <div className="email">
                                     <div className="label">
                                         <input className="checkbox" type="checkbox" name="phone-email" id="email" value="Email" />
                                         <label htmlFor="email">Email Address</label><br />
                                     </div>
-                                    <input className="text" type="email" name="email" id="email" placeholder="johndoe1@email.com"/><br />
+                                    <input 
+                                        className="text" 
+                                        type="email" 
+                                        name="email" 
+                                        id="email" 
+                                        placeholder="johndoe1@email.com"
+                                        value={email}
+                                        onChange={handleChange}
+                                    />
+                                    <br />
                                 </div>
                             </div>
                         </div>
                         <div className="request-options">
-                            <label htmlFor="option">Please select an option below:</label><br />
+                            <label>Please select an option below:</label><br />
                             <div className="request-row">
                                 <div>
-                                    <input className="radio" type="radio" name="option" id="question" value="Questions"/>
-                                    <label htmlFor="question">Question</label><br />
+                                    <input 
+                                        className="radio" 
+                                        type="radio" 
+                                        name="option" 
+                                        id="question" 
+                                        value="Question"
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="question">Question</label>
+                                    <br />
                                 </div>
                                 <div>
-                                    <input className="radio" type="radio" name="option" id="comment" value="Comments"/>
-                                    <label htmlFor="comment">Comment</label><br />
+                                    <input 
+                                        className="radio" 
+                                        type="radio" 
+                                        name="option" 
+                                        id="comment" 
+                                        value="Comment"
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="comment">Comment</label>
+                                    <br />
                                 </div>
                                 <div>
-                                    <input className="radio" type="radio" name="option" id="baptism" value="Baptism"/>
-                                    <label htmlFor="baptism">Baptism</label><br />
+                                    <input 
+                                        className="radio" 
+                                        type="radio" 
+                                        name="option" 
+                                        id="baptism" 
+                                        value="Baptism"
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="baptism">Baptism</label>
+                                    <br />
                                 </div>
                                 <div>
-                                    <input className="radio" type="radio" name="option" id="prayer" value="Prayer"/>
-                                    <label htmlFor="prayer">Prayer</label><br />
+                                    <input 
+                                        className="radio" 
+                                        type="radio" 
+                                        name="option" 
+                                        id="prayer" 
+                                        value="Prayer"
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="prayer">Prayer</label>
+                                    <br />
                                 </div>
                                 <div>
-                                    <input className="radio" type="radio" name="option" id="join" value="JoinTheKingdom"/>
-                                    <label htmlFor="join">Join The Kingdom</label><br />
+                                    <input 
+                                        className="radio" 
+                                        type="radio" 
+                                        name="option" 
+                                        id="join" 
+                                        value="Join"
+                                        onChange={handleChange}
+                                    />
+                                    <label htmlFor="join">Join The Kingdom</label>
+                                    <br />
                                 </div>
                                 
                             </div>
                         </div>
                         <div className="textarea">
                             <label htmlFor="text">Please let us know what we can do for you below:</label><br />
-                            <textarea name="text" id="text" placeholder="Please enter text here." required></textarea>
+                            <textarea 
+                                name="text" 
+                                id="text" 
+                                placeholder="Please enter text here." 
+                                value={text}
+                                onChange={handleChange}
+                                required
+                            />
                         </div>
                         <input className="button" type="submit" value="Submit" />
                     </form>
