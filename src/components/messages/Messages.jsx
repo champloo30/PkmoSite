708290f48c3ec435900e98bbc66ab28a60f2f2da
champloo30 <djld30@gmail.com>
2022-01-07 00:43:03 -0500

    location page mobile responsive, working on horizontal scroll for mobile message page

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -18,12 +18,12 @@ export default function Messages() {
                     <div className="current-month">
                         <h2>January 2022</h2>
                         <div className="carousel">
-                            <div className="carousel-video">
-                                <iframe title="1/2/22" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F4682791145149503%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                            <div className="video-container">
+                            <iframe title='="1/2/22' src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F428229325673500%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen={false}></iframe>
                                 <h4>January 2, 2022</h4>
                             </div>
-                            <div className="carousel-video">
-                                <div className="video">
+                            <div className="video-container">
+                                <div className="nca">
                                     <h3>No Content Available</h3>
                                 </div>
                                 <h4>N/A</h4>
@@ -33,16 +33,16 @@ export default function Messages() {
                     <div className="previous-month">
                         <h2>Decmeber 2021</h2>
                         <div className="carousel">
-                            <div className="carousel-video">
-                                <iframe title="12/5/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F1289352224871801%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                            <div className="video-container">
+                                <iframe title="12/5/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F1289352224871801%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen={false}></iframe>
                                 <h4>December 5, 2021</h4>
                             </div>
-                            <div className="carousel-video">
-                                <iframe title="12/12/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F4186449218126185%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                            <div className="video-container">
+                                <iframe title="12/12/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F4186449218126185%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen={false}></iframe>
                                 <h4>December 12, 2021</h4>
                             </div>
-                            <div className="carousel-video">
-                                <iframe title="12/19/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F1139558456848214%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                            <div className="video-container">
+                                <iframe title="12/19/21" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FPeaceMissionaryBaptistChurch%2Fvideos%2F1139558456848214%2F&show_text=false&width=267&t=0" width="304" height="440" className="video" scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen={false}></iframe>
                                 <h4>December 19, 2021</h4>
                             </div>
                         </div>
