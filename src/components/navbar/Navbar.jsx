cbb82b89894d3cb1cccbe89dd4a92c96b2f92cc1
champloo30 <djld30@gmail.com>
2022-02-20 22:17:15 -0500

    updated nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "../../scss/navbar.scss"
-import NavLogo from "../../img-icon/PKMO-Logo-Nav.png"
+import NavLogo from "../../img-icon/PKMO-Globe-Logo.png"
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
 
@@ -16,7 +16,14 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
         <div className={"navbar " + (menuOpen && "on")} onScroll={changeNavbarColor}>
             <div className="container">
                 <div className="left">
-                    <a className="logo-sec" href="/"><img src={NavLogo} alt="Peace Kingdom Ministries of Ohio Logo" /></a>
+                    <a className="logo-sec" href="/">
+                        <img src={NavLogo} alt="Peace Kingdom Ministries of Ohio Globe Logo" className="logo-nav"/>
+                        <div className="logo-text">
+                            <h3>Peace Kingdom</h3>
+                            <h3>Ministries of</h3>
+                            <h3>Ohio</h3>
+                        </div>
+                    </a>
                 </div>
                 <div className="right">
                     <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
